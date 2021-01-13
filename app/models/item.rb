fcd35cba82a8ae9368cd5518f00e741793a381b7
igorianich <luzniak9@gmail.com>
2021-01-13 14:59:41 +0200

    change { where(user_id: owner_id) } { where(owner_id: owner_id) }

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,5 +6,5 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { in: 3..20 }
   validates :price, numericality: { greater_than: 0 }
   scope :by_city, ->(city_name) { joins(user: :city).where(cities: { name: city_name }) }
-  scope :by_owner, ->(owner_id) { where(user_id: owner_id) }
+  scope :by_owner, ->(owner_id) { where(owner_id: owner_id) }
 end
