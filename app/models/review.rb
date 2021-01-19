3c45cdd42f6a3d6bdc693fef00d8018b44d36bfa
igorianich <luzniak9@gmail.com>
2021-01-19 15:49:34 +0200

    Change name of validate in review model Improve test for user

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,18 +3,19 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: 'User'
 
   validates :text, presence: true, length: { in: 10..1000 }
-  validate :was_booking
+  validate :rights_to_live_review
 
   private
 
-  def was_booking
+  def rights_to_live_review
     case reviewable_type
     when 'Item'
       if Booking.where(borrower_id: reviewer_id, item_id: reviewable_id).empty?
         errors.add(:reviewer, "don't have bookings")
       end
     when 'User'
-      if Booking.joins(:item).where(borrower_id: reviewer_id, items: { owner_id: reviewable_id }).empty?
+      if Booking.joins(:item).where(borrower_id: reviewer_id,
+                                    items: { owner_id: reviewable_id }).empty?
         errors.add(:reviewer, "don't work with user")
       end
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,76 +4,78 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 RSpec.describe User, type: :model do
-  let!(:city) { create(:city) }
-  let!(:first_name) { 'Petro' }
-  let!(:last_name) { 'Tarasov' }
-  let!(:age) { 21 }
-  let!(:adress) { 'Zhopa mira' }
-  subject do
-    build(
-      :user, first_name: first_name, last_name: last_name, age: age,
-      city: city, adress: adress
-    )
-  end
+  describe '#validate' do
+    subject { user.validate }
 
-  context 'when user is valid' do
-    it 'is valid' do
-      expect(subject.validate).to be true
+    let(:city) { create(:city) }
+    let(:first_name) { 'Petro' }
+    let(:last_name) { 'Tarasov' }
+    let(:age) { 21 }
+    let(:adress) { 'Zhopa mira' }
+    let(:user) do
+      build(
+        :user,
+        first_name: first_name, last_name: last_name, age: age,
+        city: city, adress: adress
+      )
     end
-  end
-  shared_examples :invalid_user do |error_key|
-    it 'is not valid' do
-      expect(subject.validate).to be false
-      expect(subject.errors.keys).to include(error_key)
+
+    context 'when user is valid' do
+      it 'is valid' do
+        expect(subject).to be true
+      end
     end
-  end
 
-  shared_examples :users_error do |error_key, error_text|
-    it 'is not valid' do
-      expect(subject.validate).to be false
-      expect(subject.errors[error_key][0]).to eq(error_text)
+    shared_examples :invalid_user do |error_key, error_text = nil|
+      it 'is not valid' do
+        expect(subject).to be false
+        expect(user.errors.keys).to include(error_key)
+        expect(user.errors[error_key][0]).to eq(error_text) if error_text
+      end
     end
-  end
 
-  context 'when user does not have first name' do
-    let(:first_name) { nil }
+    context 'when user does not have first name' do
+      let(:first_name) { nil }
 
-    it_behaves_like :invalid_user, :first_name
-  end
+      it_behaves_like :invalid_user, :first_name
+    end
 
-  context 'when users first name is too short ' do
-    let(:first_name) { 'd' }
+    context 'when users first name is too short ' do
+      let(:first_name) { 'd' }
 
-    it_behaves_like :users_error, :first_name, 'is too short (minimum is 2 characters)'
-  end
+      it_behaves_like :invalid_user,
+                      :first_name, 'is too short (minimum is 2 characters)'
+    end
 
-  context 'when user does not have first name' do
-    let(:last_name) { nil }
+    context 'when user does not have first name' do
+      let(:last_name) { nil }
 
-    it_behaves_like :invalid_user, :last_name
-  end
+      it_behaves_like :invalid_user, :last_name
+    end
 
-  context 'when users last name is too short' do
-    let(:last_name) { 'd' }
+    context 'when users last name is too short' do
+      let(:last_name) { 'd' }
 
-    it_behaves_like :users_error, :last_name, 'is too short (minimum is 2 characters)'
-  end
+      it_behaves_like :invalid_user,
+                      :last_name, 'is too short (minimum is 2 characters)'
+    end
 
-  context 'when user does not have age' do
-    let(:age) { nil }
+    context 'when user does not have age' do
+      let(:age) { nil }
 
-    it_behaves_like :invalid_user, :age
-  end
+      it_behaves_like :invalid_user, :age
+    end
 
-  context 'when users age less than 18' do
-    let(:age) { 17 }
+    context 'when users age less than 18' do
+      let(:age) { 17 }
 
-    it_behaves_like :users_error, :age, 'must be greater than 17'
-  end
+      it_behaves_like :invalid_user, :age, 'must be greater than 17'
+    end
 
-  context 'when user does not have city' do
-    let(:city) { nil }
+    context 'when user does not have city' do
+      let(:city) { nil }
 
-    it_behaves_like :invalid_user, :city
+      it_behaves_like :invalid_user, :city
+    end
   end
 end
